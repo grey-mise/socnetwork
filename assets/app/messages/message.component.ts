0187f208239ebddc3528c64d8bb178f161211600
tanchik <mailtanuxa@rambler.ru>
2017-07-31 16:42:53 +0300

    try components

---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from "@angular/core";
+import {Message} from "./message.model";
+
+@Component({
+    selector: 'app-message',
+    templateUrl: './message.component.html',
+    styleUrls: ['./message.component.css']
+})
+
+export class MessageComponent {
+    @Input() message: Message;
+}
--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
-});
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Tanya',
-        lastName: 'Tan',
-        email: email,
-        password: 'pass'
-    });
-    user.save();
-    res.redirect('/');
-});
-
-router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+   res.render('index');
 });
 
 module.exports = router;
