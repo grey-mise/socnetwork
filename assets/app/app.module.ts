41a6ae8fb5327a3ef50c14aee106fd5d4e547021
tanchik <mailtanuxa@rambler.ru>
2017-09-11 18:44:10 +0300

    crud backend for message

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from "./app.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MessageComponent} from "./messages/message.component";
 import {MessageListComponent} from "./messages/message-list.component";
-import {MessageInputComponent} from "./messages/message-input";
 import {AuthenticationComponent} from "./auth/authentication.component";
 import {HeaderComponent} from "./header.component";
 import {routing} from "./app.routing";
@@ -13,6 +12,8 @@ import {MessagesComponent} from "./messages/messages.component";
 import {SigninComponent} from "./auth/signin.component";
 import {SignupComponent} from "./auth/signup.component";
 import {LogoutComponent} from "./auth/logout.component";
+import {HttpModule} from "@angular/http";
+import {MessageInputComponent} from "./messages/message-input.component";
 
 @NgModule({
     declarations: [
@@ -28,7 +29,13 @@ import {LogoutComponent} from "./auth/logout.component";
         SignupComponent,
         LogoutComponent
     ],
-    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing,
+        ReactiveFormsModule,
+        HttpModule
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
